8d3ed74c9252c43fe475266914fab93a04838c6f
DucThien6789 <thienld4318@ut.edu.vn>
2026-07-03 01:32:25 +0700

    database

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "he_thong_xac_thuc"; // Thay bằng tên database của bạn
+$username = "root";               
+$password = "";                   
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Kết nối cơ sở dữ liệu thất bại: " . $e->getMessage());
+}
+?>
